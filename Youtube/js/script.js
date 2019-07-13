eba0731a21c52b87d4ca222cc694f540557781de
carlatin <robinsonemanuelopez@gmail.com>
2019-07-13 11:09:21 -0600

    youtube container

--- Youtube/js/script.js
+++ Youtube/js/script.js
@@ -1,6 +1,47 @@
 var app=angular.module("app",[]);
 app.controller("control",function($scope){
+	$scope.m=true;
 	$scope.video1=function(x){
+		if(x==1){
 			$scope.n=true;
+			$scope.x=false;
+			$scope.y=false;
+			$scope.z=false;
+			$scope.m=false;
+			$("#a1").addClass("ec");
+			$("#a2").removeClass("ec2");
+			$("#a3").removeClass("ec3");
+			$("#a4").removeClass("ec");
+		}else if(x==2){
+			$scope.x=true;
+			$scope.n=false;
+			$scope.y=false;
+			$scope.z=false;
+			$scope.m=false;
+			$("#a2").addClass("ec2");
+			$("#a1").removeClass("ec");
+			$("#a3").removeClass("ec3");
+			$("#a4").removeClass("ec");
+		}else if(x==3){
+			$scope.y=true;
+			$scope.x=false;
+			$scope.n=false;
+			$scope.z=false;
+			$scope.m=false;
+			$("#a3").addClass("ec3");
+			$("#a1").removeClass("ec");
+			$("#a2").removeClass("ec2");
+			$("#a4").removeClass("ec");
+		}else if(x==4){
+			$scope.z=true;
+			$scope.x=false;
+			$scope.y=false;
+			$scope.n=false;
+			$scope.m=false;
+			$("#a4").addClass("ec");
+			$("#a1").removeClass("ec");
+			$("#a3").removeClass("ec3");
+			$("#a2").removeClass("ec2");
+		}
 	}
 });
